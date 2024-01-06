6572c58e5d101ae65bb36880d43a8e878b526e76
sreelakshmi <dsreelakshmihoney@gmail.com>
2024-01-06 14:34:43 +0530

    pushed successful

---
+++ src/main/java/SEGroup/octSrilakshmijava/App.java
@@ -0,0 +1,13 @@
+package SEGroup.octSrilakshmijava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello today is 31 oct" );
+    }
+}
